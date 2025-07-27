3502571a1f950a6b0943b1c7e8c4d946baa46b65
BasaiCorp <basaicorp06@gmail.com>
2025-07-27 09:39:17 +0530

    update

--- razen.js
+++ razen.js
@@ -1,9 +1,52 @@
 Prism.languages.razen = {
-	'comment': /#.*/,
-	'string': /"(?:\\.|[^"\\])*"/,
-	'boolean': /\b(?:true|false|null)\b/,
+	// Comments
+	'comment': {
+		pattern: /#.*/,
+		greedy: true
+	},
+	
+	// Strings
+	'string': {
+		pattern: /"(?:\\.|[^"\\])*"/,
+		greedy: true
+	},
+	
+	// Numbers
 	'number': /\b\d+(?:\.\d+)?\b/,
-	'keyword': /\b(?:num|str|bool|var|const|if|else|while|for|is|when|not|list|arr|append|remove|map|key|value|store|box|ref|show|read|fun|async|await|class|return|continue|break|import|export|use|from|to|lib|enum|struct|match)\b/,
-	'operator': /->|=>|==|!=|<=|>=|&&|\|\||[+\-*/%]/,
-	'punctuation': /[{}[\];(),.:]/
+	
+	// Booleans and null
+	'boolean': /\b(?:true|false|null)\b/,
+	
+	// Declaration keywords
+	'declaration': /\b(?:num|str|bool|var|const|fun|class|enum|struct|list|arr|map)\b/,
+	
+	// Control flow keywords
+	'control-flow': /\b(?:if|else|while|for|when|match|return|continue|break)\b/,
+	
+	// Data structure operations
+	'data-structure': /\b(?:append|remove|key|value|store|box|ref)\b/,
+	
+	// I/O operations
+	'io': /\b(?:show|read)\b/,
+	
+	// Async keywords
+	'async': /\b(?:async|await)\b/,
+	
+	// Module/import keywords
+	'module': /\b(?:import|export|use|from|to|lib)\b/,
+	
+	// Logical keywords
+	'logical': /\b(?:is|not)\b/,
+	
+	// Operators
+	'operator': /->|=>|==|!=|<=|>=|&&|\|\||[+\-*/%=<>]/,
+	
+	// Punctuation
+	'punctuation': /[{}[\];(),.:]/,
+	
+	// Function calls (identifiers followed by parentheses)
+	'function': /\b[a-zA-Z_]\w*(?=\s*\()/,
+	
+	// Variables and identifiers
+	'variable': /\b[a-zA-Z_]\w*\b/
 };
