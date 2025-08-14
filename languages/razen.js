2e9291014eb4acdabf93ba8f45ac908ebae17b24
BasaiCorp <basaicorp06@gmail.com>
2025-08-14 21:36:10 +0530

    update razen.js

--- languages/razen.js
+++ languages/razen.js
@@ -1,80 +1,107 @@
-monaco.languages.register({ id: 'razen' });
-
-monaco.languages.setMonarchTokensProvider('razen', {
-    keywords: [
-        'num', 'str', 'bool', 'var', 'const', 'if', 'else', 'while', 'for', 'is', 'when', 'not',
-        'list', 'arr', 'append', 'remove', 'map', 'key', 'value', 'store', 'box', 'ref', 'show',
-        'read', 'fun', 'async', 'await', 'class', 'return', 'continue', 'break', 'import',
-        'export', 'use', 'from', 'to', 'lib', 'true', 'false', 'null'
-    ],
-
-    operators: [
-        '=', '>', '<', '!', '~', '?', ':', '==', '<=', '>=', '!=',
-        '&&', '||', '++', '--', '+', '-', '*', '/', '&', '|', '^', '%',
-        '->', '=>'
-    ],
-
-    // we include these common regular expressions
-    symbols:  /[=><!~?:&|+\-*\/\^%]+/,
-
-    // C# style strings
-    escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
-
-    tokenizer: {
-        root: [
-            // identifiers and keywords
-            [/[a-zA-Z_]\w*/, {
-                cases: {
-                    '@keywords': 'keyword',
-                    '@default': 'identifier'
-                }
-            }],
-
-            // whitespace
-            { include: '@whitespace' },
-
-            // delimiters and operators
-            [/[{}()\[\]]/, '@brackets'],
-            [/[<>](?!@symbols)/, '@brackets'],
-            [/@symbols/, {
-                cases: {
-                    '@operators': 'operator',
-                    '@default': ''
-                }
-            }],
-
-            // numbers
-            [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
-            [/0[xX][0-9a-fA-F]+/, 'number.hex'],
-            [/\d+/, 'number'],
-
-            // delimiter: after number because of .\d floats
-            [/[;,.]/, 'delimiter'],
-
-            // strings
-            [/"([^"\\]|\\.)*$/, 'string.invalid' ],  // non-teminated string
-            [/"/,  { token: 'string.quote', bracket: '@open', next: '@string' } ],
-
-            // characters
-            [/'[^\\']'/, 'string'],
-            [/(')(@escapes)(')/, ['string','string.escape','string']],
-            [/'/, 'string.invalid']
+// Wait until Monaco is loaded
+if (typeof monaco === 'undefined') {
+    console.error("Monaco is not loaded yet!");
+} else {
+    // Register the language
+    monaco.languages.register({
+        id: 'razen',
+        extensions: ['.rzn'],
+        aliases: ['Razen', 'razen'],
+    });
+    
+    // Set language configuration
+    monaco.languages.setLanguageConfiguration('razen', {
+        comments: {
+            lineComment: '#',
+        },
+        brackets: [
+            ['{', '}'],
+            ['[', ']'],
+            ['(', ')'],
         ],
-
-        comment: [
-            [/#.*$/, 'comment']
+        autoClosingPairs: [
+            { open: '{', close: '}' },
+            { open: '[', close: ']' },
+            { open: '(', close: ')' },
+            { open: '"', close: '"' },
+            { open: "'", close: "'" },
         ],
-
-        string: [
-            [/[^\\"]+/, 'string'],
-            [/@escapes/, 'string.escape'],
-            [/\\./, 'string.escape.invalid'],
-            [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' } ]
+        surroundingPairs: [
+            { open: '{', close: '}' },
+            { open: '[', close: ']' },
+            { open: '(', close: ')' },
+            { open: '"', close: '"' },
+            { open: "'", close: "'" },
         ],
-
-        whitespace: [
-            [/[ \t\r\n]+/, 'white'],
-            [/#.*$/, 'comment'],
+    });
+    
+    // Set tokenizer
+    monaco.languages.setMonarchTokensProvider('razen', {
+        keywords: [
+            'num', 'str', 'bool', 'var', 'const', 'if', 'else', 'while', 'for', 'is', 'when', 'not',
+            'list', 'arr', 'append', 'remove', 'map', 'key', 'value', 'store', 'box', 'ref', 'show',
+            'read', 'fun', 'async', 'await', 'class', 'return', 'continue', 'break', 'import',
+            'export', 'use', 'from', 'to', 'lib', 'true', 'false', 'null'
         ],
-    },
-});
+        operators: [
+            '=', '>', '<', '!', '~', '?', ':', '==', '<=', '>=', '!=',
+            '&&', '||', '++', '--', '+', '-', '*', '/', '&', '|', '^', '%',
+            '->', '=>'
+        ],
+        symbols: /[=><!~?:&|+\-*\/\^%]+/,
+        escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+        
+        tokenizer: {
+            root: [
+                // Line comments
+                [/#.*$/, 'comment'],
+                
+                // Whitespace
+                [/[ \t\r\n]+/, ''],
+                
+                // Numbers
+                [/\d*\.\d+([eE][-+]?\d+)?/, 'number.float'],
+                [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+                [/\d+/, 'number'],
+                
+                // Strings
+                [/"([^"\\]|\\.)*$/, 'string.invalid'],
+                [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+                
+                // Char literals
+                [/'[^\\']'/, 'string.char'],
+                [/(')(@escapes)(')/, ['string.char', 'string.escape', 'string.char']],
+                [/'/, 'string.invalid'],
+                
+                // Identifiers and keywords
+                [/[a-zA-Z_]\w*/, {
+                    cases: {
+                        '@keywords': 'keyword',
+                        '@default': 'identifier'
+                    }
+                }],
+                
+                // Brackets
+                [/[{}()\[\]]/, '@brackets'],
+                
+                // Operators and delimiters
+                [/@symbols/, {
+                    cases: {
+                        '@operators': 'operator',
+                        '@default': 'delimiter'
+                    }
+                }],
+                
+                // Delimiters
+                [/[;,.]/, 'delimiter'],
+            ],
+            
+            string: [
+                [/[^\\"]+/, 'string'],
+                [/@escapes/, 'string.escape'],
+                [/\\./, 'string.escape.invalid'],
+                [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }]
+            ],
+        }
+    });
+}
